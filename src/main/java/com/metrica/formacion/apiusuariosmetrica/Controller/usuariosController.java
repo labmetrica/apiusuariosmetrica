0f408daa4ae9e6c053f95b2df62fa5e992c659ef
artfern95 <36283497+artfern95@users.noreply.github.com>
2019-11-11 13:45:22 +0100

    añadido properties para comunicacion con angular

--- src/main/java/com/metrica/formacion/apiusuariosmetrica/Controller/usuariosController.java
+++ src/main/java/com/metrica/formacion/apiusuariosmetrica/Controller/usuariosController.java
@@ -3,14 +3,7 @@ package com.metrica.formacion.apiusuariosmetrica.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.metrica.formacion.apiusuariosmetrica.Service.usuariosService;
 import com.metrica.formacion.apiusuariosmetrica.entity.usuarios;
@@ -18,6 +11,7 @@ import com.metrica.formacion.apiusuariosmetrica.error.BuscarIdNotFoundException;
 
 import lombok.extern.log4j.Log4j2;
 
+@CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
 @Log4j2
 @RestController
 @RequestMapping("/clientes")
