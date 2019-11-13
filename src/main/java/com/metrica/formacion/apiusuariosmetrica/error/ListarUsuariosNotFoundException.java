c0f3b9d66416c0e3443b01a5841ab9cd272e504c
artfern95 <ARTfer050609>
2019-11-13 09:47:53 +0100

    añadido gitignore

--- src/main/java/com/metrica/formacion/apiusuariosmetrica/error/ListarUsuariosNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.metrica.formacion.apiusuariosmetrica.error;
-
-public class ListarUsuariosNotFoundException extends RuntimeException {
-	public ListarUsuariosNotFoundException() {
-		super("No hay usuarios registrados ahora mismo");
-	}
-}
